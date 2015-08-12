2990518fc5793e750c3f262157194f7e5857b2a8
Yu-Ting, Kao @ Home-Ubuntu <jackzzjack@gmail.com>
2015-08-12 22:31:31 +0800

    Changelog: Add build script.
    Changelog: Add watch with ccache.
    
    Changelog: Add cscope find files.
    
    Changelog: Add build pre-required script file.

---
+++ build_pre-required.sh
@@ -0,0 +1,4 @@
+sudo apt-get install kernel-package
+
+# Reference:
+# http://www.cnblogs.com/wwang/archive/2011/01/07/1929486.html
---
+++ install.sh
@@ -0,0 +1,1 @@
+sudo dpkg -i ../linux-headers-3.13.0my-very-own-kernel_3.13.0my-very-own-kernel-10.00.Custom_amd64.deb ../linux-image-3.13.0my-very-own-kernel_3.13.0my-very-own-kernel-10.00.Custom_amd64.deb
