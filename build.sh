614f257023c56612a6ee98da9d7aeed2da27e42d
Yu-Ting, Kao <jackzzjack@gmail.com>
2016-02-23 00:51:30 +0800

    Add timestamp at build prefix.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-sudo make-kpkg -j 3 --initrd --append-to-version=my-very-own-kernel kernel-image kernel-headers
+sudo make-kpkg -j 3 --initrd --append-to-version=my-very-own-kernel-`date +%F+%H-%M-%S` kernel-image kernel-headers
